9114482652f927e6e13ffd6de8f70aea9bc7a6c5
Asorus <Asorus@DESKTOP-ODDETGD>
2024-07-17 15:08:17 -0500

    Add project files.

---
+++ player.py
@@ -0,0 +1,29 @@
+from settings import *
+import pygame as pg
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+        
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+
+    
+    def update(self):
+        self.movement()
+        
+    @property
+    def pos(self):
+        return self.x, self.y
+    
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
